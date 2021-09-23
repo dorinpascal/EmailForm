85d97b45ad33ed2659158ede9c3df9c5975271b7
dorinpascal <74955991+dorinpascal@users.noreply.github.com>
2021-09-23 17:10:53 +0200

    Add project files.

---
+++ EmailForm/Program.cs
@@ -0,0 +1,43 @@
+﻿using FluentEmail.Core;
+using FluentEmail.Razor;
+using FluentEmail.Smtp;
+
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailForm
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var sender = new SmtpSender(() => new System.Net.Mail.SmtpClient("localhost")
+            {
+                EnableSsl = false,
+                
+                DeliveryMethod=SmtpDeliveryMethod.SpecifiedPickupDirectory,
+                PickupDirectoryLocation=@"C:\Email"
+            }); ;
+
+
+            StringBuilder template = new();
+            template.AppendLine("Dear @Model.FirstName,");
+            template.AppendLine("<p>Thanks for purchasing @Model.ProductName.We hope you enjoy it.</p>");
+            template.AppendLine("- Dorin Pascal");
+
+
+            Email.DefaultSender = sender;
+            Email.DefaultRenderer = new RazorRenderer();
+
+            var email = await Email
+                .From("pascald@mail.ru").To("304192@via.dk", "Dorin").Subject("Thanks!").
+                UsingTemplate(template.ToString(),new { FirstName = "Dorin", ProductName = "Cars" })
+               // Body("Thanks for buying our product")
+                .SendAsync();
+           
+
+
+        }
+    }
+}
